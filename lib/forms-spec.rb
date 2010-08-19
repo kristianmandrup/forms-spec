79045de0320e1e78425c2c2fe6dfc574466db9c8
Kristian Mandrup <kmandrup@gmail.com>
2010-08-19 09:47:37 +0200

    added form helper rspec macro

--- lib/forms-spec.rb
+++ lib/forms-spec.rb
@@ -2,3 +2,29 @@ require 'require_all'
 require 'code-spec'
 
 require_all File.dirname(__FILE__) + '/forms-spec'
+
+module FormHelperMacro  
+  def with_form_helper name
+    class_eval do
+      include get_form_matcher(name)
+    end
+  end
+  
+  protected
+
+  def get_form_matcher name
+    case name
+    when :formtastic
+      Erb::Formtastic::ContentMatchers
+    when :simpleform
+      Erb::SimpleForm::ContentMatchers
+    else
+      Erb::RailsForm::ContentMatchers
+    end    
+  end  
+end
+ 
+
+RSpec.configure do |config|
+  config.extend(FormHelperMacro) 
+end
